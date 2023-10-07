95a74fc6a2da119ec581f96d5356e31bec8c8c5c
ageraustine <agerausten@gmail.com>
2023-10-07 21:34:29 +0300

    guide gif

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -85,54 +85,12 @@ export class HomePage extends Component {
             </div>
           </div>
         </div>
-        <div
-          id="process"
-          class="carousel carousel-dark slide w-100"
-          data-bs-ride="carousel"
-        >
-          <div class="carousel-inner mt-2">
-            {state.steps.map((val, index) => (
-              <React.Fragment>
-                <div
-                  className={`carousel-item ${index === 0 ? "active" : ""}`}
-                  data-bs-interval="10000"
-                >
-                  <div
-                    className="mb-2 ms-1 me-1"
-                    style={{
-                      height: "15em",
-                      border: "2px solid indigo",
-                      borderRadius: "5px",
-                    }}
-                  ></div>
-                  <div class="carousel-caption mt-5 align-self-center">
-                    <h5 style={{ color: "blue" }}>
-                      {index + 1}. {val.title}
-                    </h5>
-                    <p style={{ color: "black" }}>{val.description}</p>
-                  </div>
-                </div>
-              </React.Fragment>
-            ))}
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#process"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#process"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <h4 style={{ alignSelf: "center" }}>Navigate Emit Website</h4>
+        </div>
+
+        <div className="row justify-content-center">
+          <img src="guide.gif" className="mb-2"/>
         </div>
       </div>
     );
